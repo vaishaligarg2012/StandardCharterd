6655910cd4f9bde8bd23fe03f9124d3e4d43c414
vaishaligarg <vaishaligarg2012@gmail.com>
2022-08-07 20:47:37 +0530

    initial payment config

--- src/StaticBlock.java
+++ src/StaticBlock.java
@@ -7,8 +7,6 @@ public class StaticBlock {
         System.out.println("I am from constructor");
     }
     public static void main(String[] args){
-
-        StaticBlock sb = new StaticBlock();
         System.out.println("I am from main");
 
     }
